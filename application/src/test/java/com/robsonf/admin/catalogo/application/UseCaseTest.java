316158293b5599e73ead47c08c0c0e022a26a84c
Robson Trasel <robson.trasel@gmail.com>
2023-01-12 00:10:06 -0300

    feat: add UseCaseTest base file

---
+++ application/src/test/java/com/robsonf/admin/catalogo/application/UseCaseTest.java
@@ -0,0 +1,15 @@
+package com.robsonf.admin.catalogo.application;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class UseCaseTest {
+
+    @Test
+    public void testCreateUseCase() {
+        Assertions.assertNotNull(new UseCase());
+        Assertions.assertNotNull(new UseCase().execute());
+    }
+
+
+}
