208c278218530012c775ba7eded50a0ccd3f7662
Robson Trasel <robson.trasel@gmail.com>
2023-01-12 00:35:39 -0300

    chore: setup the initial category model done

--- application/src/main/java/com/robsonf/admin/catalogo/application/UseCase.java
+++ application/src/main/java/com/robsonf/admin/catalogo/application/UseCase.java
@@ -1,5 +1,4 @@
 package com.robsonf.admin.catalogo.application;
-import com.robsonf.admin.catalogo.domain.Category;
 
 public class UseCase {
     public Category execute() {
--- domain/src/main/java/com/robsonf/admin/catalogo/domain/Category.java
+++
@@ -1,5 +0,0 @@
-package com.robsonf.admin.catalogo.domain;
-
-public class Category {
-    public String id;
-}
