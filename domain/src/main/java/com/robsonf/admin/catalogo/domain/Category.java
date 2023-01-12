a7a88fc841807bc94d3e8c27c45d8dfb41e602ac
Robson Trasel <robson.trasel@gmail.com>
2023-01-11 23:59:01 -0300

    chore: created domain, infra layer for app and added category

---
+++ domain/src/main/java/com/robsonf/admin/catalogo/domain/Category.java
@@ -0,0 +1,5 @@
+package com.robsonf.admin.catalogo.domain;
+
+public class Category {
+    public String id;
+}
--- infrastructure/src/main/java/com/robsonf/admin/catalogo/infrastructure/Main.java
+++ infrastructure/src/main/java/com/robsonf/admin/catalogo/infrastructure/Main.java
@@ -1,4 +1,4 @@
-package com.robsonf.admin.catalogo;
+package com.robsonf.admin.catalogo.infrastructure;
 
 public class Main {
     public static void main(String[] args) {
