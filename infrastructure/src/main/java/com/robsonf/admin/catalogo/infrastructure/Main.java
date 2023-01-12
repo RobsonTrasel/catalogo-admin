6d9dcf879d0a54bc3af8947ddbc3ceea03b686e0
Robson Trasel <robson.trasel@gmail.com>
2023-01-12 00:10:24 -0300

    chore: modified main base file

--- infrastructure/src/main/java/com/robsonf/admin/catalogo/infrastructure/Main.java
+++ infrastructure/src/main/java/com/robsonf/admin/catalogo/infrastructure/Main.java
@@ -1,7 +1,10 @@
 package com.robsonf.admin.catalogo.infrastructure;
 
+import com.robsonf.admin.catalogo.application.UseCase;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        System.out.println(new UseCase().execute());
     }
 }
