86c7781b93ce1c59468330e9735045a80885b095
Robson Trasel <robson.trasel@gmail.com>
2023-01-12 00:09:56 -0300

    feat: add useCase base file

---
+++ application/src/main/java/com/robsonf/admin/catalogo/application/UseCase.java
@@ -0,0 +1,9 @@
+package com.robsonf.admin.catalogo.application;
+import com.robsonf.admin.catalogo.domain.Category;
+
+public class UseCase {
+    public Category execute() {
+        return new Category();
+    }
+
+}
